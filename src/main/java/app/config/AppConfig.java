e7a6e88ce529d33f5b49289ab3fe0e95e43e9494
Cergonchik <152849771+CergeyGoncharov@users.noreply.github.com>
2023-12-23 12:13:23 +0300

    SpringBeans 1.0

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,22 @@
 package app.config;
 
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+//    @Bean(name = "dog")
+//    public Dog getDog(){
+//        return new Dog();
+//    }
+//    @Bean(name = "timer")
+//    public Timer getTimer() {
+//        return new Timer();
+//    }
 }
